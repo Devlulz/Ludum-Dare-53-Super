3219d7b350530081081a0590c87e3a42372918d5
Omega <chaseomega@gmail.com>
2023-04-28 23:25:04 -0500

    Added scene for my development focusing on movement and gun mechanics

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField]
+    int moveSpeed;
+    [SerializeField]
+    int climbSpeed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
